cbbe346a3d18ac1401fcba394e2a10ce340966a8
Zynh Ludwig <Zynh0722@gmail.com>
2022-07-19 16:56:57 -0700

    Create the jokes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  let [joke, setJoke] = useState('');
+
+  useEffect(() => {
+    fetch('https://geek-jokes.sameerkumar.website/api?format=json')
+      .then(response => response.json())
+      .then(data => setJoke(data.joke));
+
+    console.log(joke);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-Text">
+        <div className='Text-Wrapper'>
+          {joke ? <span>{joke}</span> : <span>Loading...</span>}
+        </div>
+      </div>
     </div>
   );
 }
